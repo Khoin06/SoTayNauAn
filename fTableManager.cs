7d425d1aeeab42fd4a181a901d4ed3076c76e7e6
Khoin06 <ngokhoinsl@gmail.com>
2024-12-05 09:23:39 -0800

    1

--- fTableManager.cs
+++ fTableManager.cs
@@ -16,5 +16,39 @@ namespace SoTayNauAn
         {
             InitializeComponent();
         }
+
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void thôngTinCáNhânToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fAccountProfile f = new fAccountProfile();
+            f.ShowDialog();
+        }
+
+        private void fTableManager_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            addcook f = new addcook();
+            this.Hide();
+            f.ShowDialog();
+            this.Show();
+        }
     }
 }
