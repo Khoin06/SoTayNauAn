e6d7115dcdefc2ac847074531951fefbd4d070cf
MinhDucGPT <163520680+MinhDucGPT@users.noreply.github.com>
2024-12-20 06:00:19 +0700

    Login.cs

--- Login.cs
+++ Login.cs
@@ -1,4 +1,4 @@
-﻿using SoTayNauAn;
+using SoTayNauAn;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace QuanLyQuanCafe
 {
@@ -20,41 +22,80 @@ namespace QuanLyQuanCafe
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            main f = new main();
-            this.Hide();
-            f.ShowDialog();
-            this.Show();
+            string userName = txbUserName.Text.Trim();
+            string passWord = txbPassWord.Text.Trim();
+
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(passWord))
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin đăng nhập.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (Login(userName, passWord))
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                mainf = new main();
+                this.Hide();
+                f.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void btnExit_Click(object sender, EventArgs e)
+        private bool Login(string userName, string passWord)
         {
-            Application.Exit();
+            string connectionString = @"Data Source=MINH-DUCK\SQLEXPRESS;Initial Catalog=CookBook;Integrated Security=True";
+            string query = "SELECT * FROM Nguoidung WHERE TK = @userName AND MK = @passWord";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@userName", userName);
+                    command.Parameters.AddWithValue("@passWord", passWord);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    return reader.HasRows; // Kiểm tra nếu có dữ liệu trả về từ CSDL.
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi kết nối cơ sở dữ liệu: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            // Mở form đăng ký, ví dụ form đăng ký có tên là fRegister
+            fSignUp registerForm = new fSignUp();
+            registerForm.Show();
+        }
+
 
         private void fLogin_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (MessageBox.Show("Bạn có thật sự muốn thoát chương trình?", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            if (MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
             {
                 e.Cancel = true;
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void txbUserName_TextChanged(object sender, EventArgs e)
         {
-
+            string userName = txbUserName.Text.Trim();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void txbPassWord_TextChanged(object sender, EventArgs e)
         {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-            fSignUp f = new fSignUp();
-            this.Hide();
-            f.ShowDialog();
-            this.Show();
+            string passWord = txbPassWord.Text.Trim();
         }
     }
 }
+
+
